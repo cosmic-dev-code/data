267c152a05f2304b71524b0132ea7fd5a96b23e9
Brandon Anthony <brandonolivares.developer@gmail.com>
2025-01-01 17:19:13 -0600

    fix(date): Modify Javascript new Date object

--- 2 - FrontEnd/Vanilla/Javascript/APIs/date.js
+++ 2 - FrontEnd/Vanilla/Javascript/APIs/date.js
@@ -1,23 +1,87 @@
+/* ##########================########## */
+/* ######===--- Instanciar ---===###### */
+/* ##########================########## */
+
+// Extrae la fecha  hora actuales del dispositivo, (computadora o celular).
+const time = new Date();
+
+// Se puede establecer también de esta manera.
+new Date("Abril 12", "2021 20:26:00");
+
+/* Podemos crear un objeto del constructor "date": 
+		--- (Año).
+		--- (Mes).
+			--- Aqui los meses inician de 0 - 11.
+		--- (Día).
+		--- (Horas).
+			--- Horas en un rango de 0 a 23.
+		--- (Minutos).
+			--- Minutos en un rango de 0 a 59.
+		--- (Segundos).
+			--- Segundos en un rango de 0 a 59.
+		--- (Milisegundos).
+			--- Milisegundos en un rango de 0 a 999.
+*/
+new Date(2024, 0, 24, 23, 60, 60, 200);
+
+/* Podemos proporcionar: 
+	--- La fecha (2024-11-01).
+	--- La hora (12:00:00).
+*/
+
+// Corresponde a las (12:00 PM) del (mediodia).
+new Date("2024-11-01T12:00:00");
+
+// Corresponde a las (12:00 AM) de (medianoche), inicio del dia.
+new Date("2024-11-01T00:00:00");
+new Date(2024, 10, 1, 0, 0, 0);
+
+// Esto representa (Diciembre de 2024 del dia 30).
+new Date("2024-12-30");
+
+// NOTA: Cuando pasas la fecha en cadena (string), esta empieza en 0.
+
 /* ##########==================########## */
-/* ######===--- Fechas (UTC) ---===###### */
+/* ######===--- Converciones ---===###### */
 /* ##########==================########## */
 
-// Extrae la fecha de cadena UTC.
-const time = new Date(), 
-	// Convierte una cadena UTC en una fecha de tipo estándar.
-	newTime = time.toUTCString(),
+const date = new Date();
 
-	// Convierte la cadena UTC en un formato más legible.
-	newTime1 = time.toDateString(),
+// ------------------------------------- //
+// ------ Conversiones compleatas ------ //
+// ------------------------------------- //
 
-	// Convierte la cadena UTC utilizando el método de formato ISO.
-	newTime2 = time.toISOString(),
+// "Wed, 01 Jan 2025 20:47:38 GMT"
+date.toUTCString();
 
-	// Podemos crear un objeto del constructor 'date' con los datos establecidos.
-	timePersonalize = new Date("Año", "Mes", "Día", "Hora", "Minuto", "Segundo", "Milisegundo"),
+// "Wed Jan 01 2025"
+date.toDateString();
 
-	// Se puede establecer también de esta manera.
-	timePersonalize1 = new Date("Abril 12", "2021 20:26:00");
+/* Propociona: 
+		--- 2025-01-01
+		--- 20:49:38.413Z
+*/
+// "2025-01-01T20:49:38.413Z"
+date.toISOString();
+
+// "Wed Jan 01 2025 14:55:37 GMT-0600 (hora estándar central)"
+date.toString();
+
+// "14:56:15 GMT-0600 (hora estándar central)"
+date.toTimeString();
+
+// ---------------------------------- //
+// ------ Obtener Fecha y Hora ------ //
+// ---------------------------------- //
+
+// "1/1/2025, 2:53:25 p.m."
+date.toLocaleString();
+
+// "1/1/2025"
+date.toLocaleDateString();
+
+// "2:54:21 p.m."
+date.toLocaleTimeString();
 
 /* ##########=========================########## */
 /* ######===--- Partes de una fecha ---===###### */
@@ -25,32 +89,32 @@ const time = new Date(),
 
 const time = new Date();
 
-// extraemos la fecha completa con la hora completa.
-let milliseconds = time.getMilliseconds(), 
+// Extrae el tiempo en milisegundos desde el 1 de enero de 1970.
+time.getTime();
 
-	// Extrae los segundos.
-	seconds = time.getSeconds(), 
+// Extrae los milisegundos.
+time.getMilliseconds();
 
-	// Extrae los minutos.
-	minutes = time.getMinutes(), 
+// Extrae los segundos.
+time.getSeconds();
 
-	// Extrae las horas.
-	hours = time.getHours(), 
+// Extrae los minutos.
+time.getMinutes();
 
-	// Extrae el mes.
-	month = time.getMonth(), // 0 - 11, (empieza del 0 como enero).
+// Extrae las horas.
+time.getHours();
 
-	// Extrae el año.
-	year = time.getFullYear(), 
+// Extrae el mes.
+time.getMonth(); // 0 - 11, (empieza del 0 como enero).
 
-	// Extrae el día del mes.
-	date = time.getDate(), 
+// Extrae el año.
+time.getFullYear();
 
-	// Extrae el dia de la semana.
-	day = time.getDay(), // 1 - 7, (empieza del 1 como lunes).
+// Extrae el día del mes.
+time.getDate();
 
-	// Extrae el tiempo en milisegundos desde el 1 de enero de 1970.
-	newGetTime = time.getTime();
+// Extrae el dia de la semana.
+time.getDay(); // 1 - 7, (empieza del 1 como lunes).
 
 /* ##########================########## */
 /* ######===--- Date.now() ---===###### */
@@ -58,7 +122,7 @@ let milliseconds = time.getMilliseconds(),
 
 /* Devuelve el número de milisegundos transcurridos desde el: 
 	--- 1 de enero de 1970, 00:00:00 UTC (Coordinated Universal Time), hasta la fecha y hora actuales. */
-const milliseconds = Date.now();
+Date.now(); // number
 
 // ------------------------------------------ //
 // ------ Medir el tiempo de ejecucion ------ //
@@ -82,9 +146,7 @@ console.log(`Tiempo transcurrido: ${(start - end)} millisegundos.`);
 
 // Agrega un cero a la izquierda.
 function addZero(){
-	let numero = arguments[0];
-
-	return (numero <= 9 && numero > 0) ? ("0" + numero) : `${numero}`;
+	return arguments[0].toString().padStart(2, "0");
 }
 
 addZero(6); // "06"
